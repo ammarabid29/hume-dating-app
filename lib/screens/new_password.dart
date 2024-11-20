96b89d764326f19d5df1f465cd98c2a75d8bf615
ammarabid29 <aammarawan786@gmail.com>
2024-11-20 12:22:47 +0500

    changed new password logic

--- lib/screens/new_password.dart
+++ lib/screens/new_password.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg_icons/flutter_svg_icons.dart';
 import 'package:hume_dating_app/screens/invite_friends.dart';
-import 'package:hume_dating_app/widgets/text_field.dart';
 
 class NewPasswordScreen extends StatefulWidget {
   const NewPasswordScreen({super.key});
@@ -11,9 +11,33 @@ class NewPasswordScreen extends StatefulWidget {
 }
 
 class _NewPasswordScreenState extends State<NewPasswordScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _password1Controller = TextEditingController();
+  final _password2Controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _password1Controller.dispose();
+    _password2Controller.dispose();
+    super.dispose();
+  }
+
+  var isShown1 = false;
+  var isShown2 = false;
+
+  void showPassword1() {
+    setState(() {
+      isShown1 = !isShown1;
+    });
+  }
+
+  void showPassword2() {
+    setState(() {
+      isShown2 = !isShown2;
+    });
+  }
+
   var isChecked = false;
-  var password1 = '';
-  var password2 = '';
 
   @override
   Widget build(BuildContext context) {
@@ -58,17 +82,92 @@ class _NewPasswordScreenState extends State<NewPasswordScreen> {
                 ),
               ),
               const SizedBox(height: 24),
-              TextFieldWidget(
-                password: (password) {
-                  password1 = password;
-                },
-              ),
-              const SizedBox(height: 24),
-              TextFieldWidget(
-                password: (password) {
-                  password2 = password;
-                },
-              ),
+              Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: _password1Controller,
+                        validator: (value) {
+                          return (value == null || value.length < 6)
+                              ? 'Enter a valid password'
+                              : null;
+                        },
+                        textAlignVertical: TextAlignVertical.center,
+                        obscureText: !isShown1,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: const Color(0xFFFAFAFA),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide.none,
+                          ),
+                          prefixIcon: IconButton(
+                            iconSize: 20,
+                            icon: const SvgIcon(
+                              icon: SvgIconData('assets/images/Lock.svg'),
+                            ),
+                            onPressed: () {},
+                          ),
+                          suffixIcon: IconButton(
+                            iconSize: 20,
+                            icon: const SvgIcon(
+                              icon: SvgIconData('assets/images/Hide.svg'),
+                            ),
+                            onPressed: showPassword1,
+                          ),
+                        ),
+                        keyboardType: TextInputType.text,
+                      ),
+                      const SizedBox(height: 24),
+                      TextFormField(
+                        controller: _password2Controller,
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              value.length < 6) {
+                            return 'Enter a valid password';
+                          } else if (value != _password1Controller.text) {
+                            return 'Paswords do not match';
+                          }
+                          return null;
+                        },
+                        textAlignVertical: TextAlignVertical.center,
+                        obscureText: !isShown2,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: const Color(0xFFFAFAFA),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide.none,
+                          ),
+                          prefixIcon: IconButton(
+                            iconSize: 20,
+                            icon: const SvgIcon(
+                              icon: SvgIconData('assets/images/Lock.svg'),
+                            ),
+                            onPressed: () {},
+                          ),
+                          suffixIcon: IconButton(
+                            iconSize: 20,
+                            icon: const SvgIcon(
+                              icon: SvgIconData('assets/images/Hide.svg'),
+                            ),
+                            onPressed: showPassword2,
+                          ),
+                        ),
+                        keyboardType: TextInputType.text,
+                      ),
+                    ],
+                  )),
               const SizedBox(height: 24),
               Center(
                 child: Row(
@@ -119,35 +218,12 @@ class _NewPasswordScreenState extends State<NewPasswordScreen> {
                     foregroundColor: Colors.white,
                   ),
                   onPressed: () {
-                    if (password1.length < 6 || password1.trim().isEmpty) {
-                      ScaffoldMessenger.of(context).clearSnackBars();
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text(
-                              "Please enter valid passwords in both feilds"),
-                        ),
-                      );
-                    } else if (password1.length < 6 ||
-                        password1.trim().isEmpty) {
-                      ScaffoldMessenger.of(context).clearSnackBars();
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text(
-                              "Please enter valid passwords in both feilds"),
-                        ),
-                      );
-                    } else if (password1 == password2) {
+                    if (_formKey.currentState!.validate()) {
+                      _password1Controller.clear();
+                      _password2Controller.clear();
                       Navigator.of(context).push(
                         MaterialPageRoute(
                           builder: (ctx) => const InviteFriendsScreen(),
-                        ),
-                      );
-                    } else {
-                      ScaffoldMessenger.of(context).clearSnackBars();
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text(
-                              "Please enter valid passwords in both feilds"),
                         ),
                       );
                     }
